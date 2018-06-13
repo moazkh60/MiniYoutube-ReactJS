a1a15c4f16b79c62924d348001c980c3d2b622f6
Moaz Khan <moazkh60@gmail.com>
2018-06-13 12:32:24 +0500

    Components created. YoutubeAPI setup
    Components created. YoutubeAPI setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDbJJz4nkKQv3CtQ7xT7UWpOG7FY_WD45E';
+
 // Creating a component
 const MyApp = () =>  {
 	return <div> Starting MiniYoutube </div>;  
