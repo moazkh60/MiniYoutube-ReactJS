991d15faddcbff31d9f6e5c559eddc594d8db39b
Moaz Khan <moazkh60@gmail.com>
2018-06-19 16:23:40 +0500

    Search bar set with loadash.
    Search bar set with loadash.

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({video}) => {
 	const url = 'https://www.youtube.com/embed/'+videoId;
 
 	return (
-		<div className="video-detal col-md8">
+		<div className="video-detail col-md-8">
 				<div className="embed-responsive embed-responsive-16by9">
 					<iframe className="embed-responsive-item" src={url}></iframe>
 				</div>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'loadash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -20,7 +21,11 @@ class MyApp extends Component{
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'swift 4'}, (videos) => {
+		this.videoSearch('Swift 4');
+	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({
 				videos: videos,
 				selectedVideo: videos[0]
@@ -30,13 +35,15 @@ class MyApp extends Component{
 
 	// rendering JSX
 	render(){
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
 		return (
 			<div>
-				<SearchBar /> 
+				<SearchBar onSearchTermChange={videoSearch} /> 
 				<VideoDetail video = {this.state.selectedVideo} />
 				<VideoList
-				onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
-				videos = {this.state.videos} />
+					onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+					videos = {this.state.videos} />
 	 		</div>
 		); 
 	}
